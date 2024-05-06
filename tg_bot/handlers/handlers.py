da0e79f175ac3476c944959b7ec0a54d7bcedd58
Ilya <105780672+fabilya@users.noreply.github.com>
2024-05-06 16:49:16 +0300

    реализовал перевод аудио в текст

--- tg_bot/handlers/handlers.py
+++ tg_bot/handlers/handlers.py
@@ -1,8 +1,11 @@
-from aiogram import Router
+import os.path
+
+import whisper
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from main import bot
 
 start_project_router = Router()
 
@@ -13,9 +16,26 @@ GREETINGS = (
 
 
 @start_project_router.message(Command('start'))
-async def command_start(message: Message, state: FSMContext):
+async def command_start(message: Message):
     """Ввод команды /start"""
     await message.answer(GREETINGS.format(message=message))
+
+
+@start_project_router.message(F.voice)
+async def save_voice_as_mp3(message: Message):
+    """
+    Downloads a voice message and saves it in mp3 format.
+    After converting audio to text, deletes the file
+    """
+    split_tup = os.path.splitext(message.voice.file_unique_id)
+    file_name = f'{split_tup[0]}_{message.from_user.full_name}.mp3'
+    await bot.download(message.voice.file_id, file_name)
+
+    model = whisper.load_model("base")
+    result = model.transcribe(file_name, fp16=False)
+    await message.answer(result['text'])
+
+    os.remove(file_name)
 
 
 
