96d1dc6c1da96432a748fba72f772af27120dbbc
Ilya <105780672+fabilya@users.noreply.github.com>
2024-05-15 07:24:43 +0300

    fix null value in postgresql

--- admin_panel/telegram/models.py
+++ admin_panel/telegram/models.py
@@ -4,17 +4,19 @@ from django.db import models
 class TgUser(models.Model):
     """Модель пользователя"""
     id = models.BigIntegerField(verbose_name='ID пользователя в telegram',
-                                primary_key=True, )
+                                primary_key=True,)
     first_name = models.CharField(verbose_name='Имя в telegram',
-                                  max_length=50,)
+                                  max_length=50,
+                                  blank=True,
+                                  null=True,)
     last_name = models.CharField(verbose_name='Фамилия в telegram',
                                  max_length=100,
                                  blank=True,
-                                 null=True, )
+                                 null=True,)
     nickname = models.CharField(verbose_name='Никнейм в telegram',
                                 max_length=50,
                                 blank=True,
-                                null=True, )
+                                null=True,)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
